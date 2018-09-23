391e7b7a8ca6201c9a18c92da7df7f1621eaa760
cholzkorn <c.holzkorn@hotmail.com>
2018-09-23 19:36:12 +0200

    Added CSS

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import bootstrap
+//import 'C:/Users/Clem/OneDrive/Informatik_Lernen/git-repos/learning-javascript/weather-app/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
 import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
 

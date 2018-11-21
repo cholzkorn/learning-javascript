579b31d5af7daca7f222e9dbbc5a80323719a371
cholzkorn <c.holzkorn@hotmail.com>
2018-11-21 22:08:39 +0100

    Finished basic challenge - could add vizualization

--- coding-challenges/is-triangle/script.js
+++ coding-challenges/is-triangle/script.js
@@ -1,9 +1,11 @@
-if ((a + b) > c && (b + c) > a && (a + c) > b) {
-  alert("True!");
-  return True;
-}
-else{
-  document.write("False");
-  return False;
-}
-}
+function is_triangle(a, b, c){
+  a = parseInt(a);
+  b = parseInt(b);
+  c = parseInt(c);
+  if ((a + b) > c && (b + c) > a && (a + c) > b) {
+    document.getElementById('output').innerHTML = "Yes, it will!";
+  }
+  else{
+    document.getElementById('output').innerHTML = "No, it won't!";
+  }
+  }

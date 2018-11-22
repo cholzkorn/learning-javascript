940ced86cbf0bd2051f4279f9efe219e1f4510f3
cholzkorn <c.holzkorn@hotmail.com>
2018-11-23 00:23:14 +0100

    Not quite there

--- coding-challenges/is-triangle/script.js
+++ coding-challenges/is-triangle/script.js
@@ -8,4 +8,31 @@ function is_triangle(a, b, c){
   else{
     document.getElementById('output').innerHTML = "No, it won't!";
   }
-  }
+};
+
+// https://www.kirupa.com/html5/drawing_triangles_on_the_canvas.htm
+function draw_triangle(a, b, c){
+  a = parseInt(a);
+  b = parseInt(b);
+  c = parseInt(c);
+
+  // selecting canvas
+  var canvasElement = document.getElementById('canvas');
+  var context = canvasElement.getContext('2d');
+
+  // the triangle
+  context.beginPath();
+  context.moveTo(100, 100);
+  context.lineTo(100, 300);
+  context.lineTo(300, 300);
+  context.closePath();
+
+  // the outline
+  context.lineWidth = 10;
+  context.strokeStyle = '#666666';
+  context.stroke();
+
+  // the fill color
+  context.fillStyle = "#FFCC00";
+  context.fill();
+}

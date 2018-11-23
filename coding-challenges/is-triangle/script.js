d7e2affac914cafb84403eced03a4ead99251ec0
cholzkorn <c.holzkorn@hotmail.com>
2018-11-23 13:07:43 +0100

    Drawing triangle works - scaling needed

--- coding-challenges/is-triangle/script.js
+++ coding-challenges/is-triangle/script.js
@@ -23,8 +23,8 @@ function draw_triangle(a, b, c){
   // the triangle
   context.beginPath();
   context.moveTo(100, 100);
-  context.lineTo(100, 300);
-  context.lineTo(300, 300);
+  context.lineTo(a, 100);
+  context.lineTo(c, b);
   context.closePath();
 
   // the outline
@@ -35,4 +35,15 @@ function draw_triangle(a, b, c){
   // the fill color
   context.fillStyle = "#FFCC00";
   context.fill();
+}
+
+function js_function(){
+   console.log("function was called");
+
+   // selecting canvas
+   var canvasElement = document.getElementById('canvas');
+   var context = canvasElement.getContext('2d');
+
+   context.font = '48px serif';
+   context.fillText('Works for me', 1, 50);
 }

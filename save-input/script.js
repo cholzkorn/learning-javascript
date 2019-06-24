4a76e6a5ca9bca00b929910833401bd80197c9bc
cholzkorn <c.holzkorn@hotmail.com>
2019-06-24 16:31:21 +0200

    added onunload functionality, client-side download not possible

--- save-input/script.js
+++ save-input/script.js
@@ -1,6 +1,7 @@
 let arr = {}
 arr.fnamefinal = "NA"
 arr.fnameunfinished = "NA"
+arr.fnameclosed = "NA"
 
 function saveFirstNameUnfinished(){
   var fnameunfinished = document.getElementById('firstname').value
@@ -14,4 +15,12 @@ function saveFirstName(){
   arr.fnamefinal = fnamefinal;
   var arrJSON = JSON.stringify(arr);
   console.log(arr);
+};
+
+function saveFirstName_closed(){
+  var fnameclosed = document.getElementById('firstname').value
+  arr.fnameclosed = fnameclosed;
+  var arrJSON = JSON.stringify(arr);
+  console.log(arr);
+  localStorage.setItem('arr.json', JSON.stringify(arr));
 };
